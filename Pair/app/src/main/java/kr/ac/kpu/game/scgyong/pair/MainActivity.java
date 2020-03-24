c3015f680d541a3abf835e560dc7bf6c2d0e7c69
Kiyong Kim <scgyong.isu@gmail.com>
2020-03-24 18:06:56 +0900

    Pair Project from AndroidStudio Wizard

---
+++ Pair/app/src/main/java/kr/ac/kpu/game/scgyong/pair/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kpu.game.scgyong.pair;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
