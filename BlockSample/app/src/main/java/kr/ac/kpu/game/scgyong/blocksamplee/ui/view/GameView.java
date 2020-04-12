09113a36e87667b3c162d5ae7623f70aafc6d171
Kiyong Kim <scgyong.isu@gmail.com>
2020-04-13 01:39:04 +0900

    added GameView

---
+++ BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/view/GameView.java
@@ -0,0 +1,10 @@
+package kr.ac.kpu.game.scgyong.blocksamplee.ui.view;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+}
