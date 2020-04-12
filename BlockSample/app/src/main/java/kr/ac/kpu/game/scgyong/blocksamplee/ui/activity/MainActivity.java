08aaaf7eff9ed0799a3b7840a481e914c783e9c4
Kiyong Kim <scgyong.isu@gmail.com>
2020-04-13 01:37:14 +0900

    added ui.activity package

--- BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/activity/MainActivity.java
+++ BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package kr.ac.kpu.game.scgyong.blocksamplee;
+package kr.ac.kpu.game.scgyong.blocksamplee.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import kr.ac.kpu.game.scgyong.blocksamplee.R;
 
 public class MainActivity extends AppCompatActivity {
 
