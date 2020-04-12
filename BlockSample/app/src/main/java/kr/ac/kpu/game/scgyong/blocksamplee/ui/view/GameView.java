be87fd5f3737eb776c6468a6d47361cbed2e09c2
Kiyong Kim <scgyong.isu@gmail.com>
2020-04-13 02:03:23 +0900

    draw ball

--- BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/view/GameView.java
+++ BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/view/GameView.java
@@ -1,8 +1,10 @@
 package kr.ac.kpu.game.scgyong.blocksamplee.ui.view;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -10,26 +12,35 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import kr.ac.kpu.game.scgyong.blocksamplee.R;
+
 public class GameView extends View {
     private Paint mainPaint;
     private Rect rect;
+    private Bitmap ballImage;
+    private int xBall, yBall;
 
     public GameView(Context context) {
         super(context);
 
-        initPaint();
+        initResources();
     }
 
-    private void initPaint() {
+    private void initResources() {
         mainPaint = new Paint();
         mainPaint.setColor(0xFFFFEEEE);
 
         rect = new Rect();
+        Resources res = getResources();
+        ballImage = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+
+        xBall = 500;
+        yBall = 600;
     }
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initPaint();
+        initResources();
     }
 
     @Override
@@ -46,5 +57,7 @@ public class GameView extends View {
         rect.bottom = h - pb;
 
         canvas.drawRect(rect, mainPaint);
+
+        canvas.drawBitmap(ballImage, xBall, yBall, null);
     }
 }
