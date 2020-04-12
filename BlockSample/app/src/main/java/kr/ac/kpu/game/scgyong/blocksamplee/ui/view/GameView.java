67b5b3b904e08a0589e12b1947c533ecdc008132
Kiyong Kim <scgyong.isu@gmail.com>
2020-04-13 01:50:54 +0900

    view load from xml

--- BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/view/GameView.java
+++ BlockSample/app/src/main/java/kr/ac/kpu/game/scgyong/blocksamplee/ui/view/GameView.java
@@ -1,10 +1,50 @@
 package kr.ac.kpu.game.scgyong.blocksamplee.ui.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 public class GameView extends View {
+    private Paint mainPaint;
+    private Rect rect;
+
     public GameView(Context context) {
         super(context);
+
+        initPaint();
+    }
+
+    private void initPaint() {
+        mainPaint = new Paint();
+        mainPaint.setColor(Color.RED);
+
+        rect = new Rect();
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initPaint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int pl = getPaddingLeft();
+        int pt = getPaddingTop();
+        int pr = getPaddingRight();
+        int pb = getPaddingBottom();
+        int w = canvas.getWidth();
+        int h = canvas.getHeight();
+        rect.left = pl;
+        rect.top = pt;
+        rect.right = w - pr;
+        rect.bottom = h - pb;
+
+        canvas.drawRect(rect, mainPaint);
     }
 }
