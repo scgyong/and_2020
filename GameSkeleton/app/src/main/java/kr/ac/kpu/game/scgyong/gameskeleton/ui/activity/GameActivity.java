bc6f62f3219555b0a2815671dce9b318f3735dfe
Kiyong Kim <scgyong@gmail.com>
2020-06-05 13:05:29 +0900

    BackPressed 처리

--- GameSkeleton/app/src/main/java/kr/ac/kpu/game/scgyong/gameskeleton/ui/activity/GameActivity.java
+++ GameSkeleton/app/src/main/java/kr/ac/kpu/game/scgyong/gameskeleton/ui/activity/GameActivity.java
@@ -3,12 +3,16 @@ package kr.ac.kpu.game.scgyong.gameskeleton.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import kr.ac.kpu.game.scgyong.gameskeleton.framework.main.UiBridge;
 import kr.ac.kpu.game.scgyong.gameskeleton.framework.view.GameView;
 import kr.ac.kpu.game.scgyong.gameskeleton.game.scene.FirstScene;
 
 public class GameActivity extends AppCompatActivity {
+
+    private static final long BACKKEY_INTERVAL_MSEC = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,23 @@ public class GameActivity extends AppCompatActivity {
         setContentView(new GameView(this));
 
         new FirstScene().run();
+
+    }
+
+    private long lastBackPressedOn;
+    @Override
+    public void onBackPressed() {
+        long now = System.currentTimeMillis();
+        long elapsed = now - lastBackPressedOn;
+        if (elapsed <= BACKKEY_INTERVAL_MSEC) {
+            finish();
+            return;
+        }
+        Log.d("BackKey", "elapsed="+elapsed);
+        Toast.makeText(this,
+                "Press Back key twice quickly to exit",
+                Toast.LENGTH_SHORT)
+                .show();
+        lastBackPressedOn = now;
     }
 }
